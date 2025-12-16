7ca31bc1955cf5dab8e338f2af6b2a0feb1fccd3
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-12-16 18:27:16 +1300

    fix: reverted to og for now

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    // Ignore thread-stream test files, docs, and other non-runtime files
-    config.module.rules.push({
-      test: /thread-stream\/.*\/(test|README|LICENSE)\/?.*\.(ts|js|mjs|md|zip|yml)?$/,
-      use: "null-loader",
-    });
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default withPayload(nextConfig);
