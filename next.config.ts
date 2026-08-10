d989df0f37edc42ea05743ae02b2a1a873c3b0d7
Andrew Chen <ache337@aucklanduni.ac.nz>
2026-08-10 19:20:42 +1200

    fix: cloudlare

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import { withPayload } from "@payloadcms/next/withPayload";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pino", "thread-stream", "pino-pretty"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
-export default withPayload(nextConfig);
+export default initOpenNextCloudflareForDev(withPayload(nextConfig));
