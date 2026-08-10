217912f68b988e3318948b1667a1c0b4e4d74423
Andrew Chen <ache337@aucklanduni.ac.nz>
2026-08-10 19:35:20 +1200

    fix: revert prev changes:

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import { withPayload } from "@payloadcms/next/withPayload";
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pino", "thread-stream", "pino-pretty"],
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
-export default initOpenNextCloudflareForDev(withPayload(nextConfig));
+export default withPayload(nextConfig);
